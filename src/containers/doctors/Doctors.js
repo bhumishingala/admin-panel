07af7804dfba6375b7b7c4d01ae7ee8c48a7c65c
bhumishingala <bhumishingala>
2022-07-13 15:30:07 +0530

    CRUD

--- src/containers/doctors/Doctors.js
+++ src/containers/doctors/Doctors.js
@@ -8,23 +8,37 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import { DataGrid } from '@mui/x-data-grid';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 function Doctors(props) {
     const [open, setOpen] = React.useState(false);
-    const [data , setData] = useState([]);
+    const [data, setData] = useState([]);
+    const [dopen, setDOpen] = React.useState(false);
+    const [did, setDid] = useState(0);
+    const [update, setUpdate] = useState(false);
+    const [search ,setSerch ] = useState([ ]);
 
     const handleClickOpen = () => {
         setOpen(true);
     };
 
+    const handleDClickOpen = () => {
+        setDOpen(true);
+    };
+
     const handleClose = () => {
         setOpen(false);
+        setDOpen(false);
+        setUpdate(false);
+        formik.resetForm();
     };
 
     let schema = yup.object().shape({
         name: yup.string().required("Please Enter Your Name."),
         email: yup.string().email("Please Enter Vaild Email Id.").required("Please Enter Email Id."),
-        phone: yup.string().matches(/^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/, 'Enter a valid phone number').min(10, "too short").required('Phone number is required'),
+        phone: yup.string().matches(/^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/, 'Enter a valid phone number').min(10, "too short").required('Phone number is required')
     });
 
     const formik = useFormik({
@@ -35,7 +49,11 @@ function Doctors(props) {
         },
         validationSchema: schema,
         onSubmit: values => {
-            handleInsert(values);
+            if (update) {
+                handleClickUpdate(values);
+            } else {
+                handleInsert(values);
+            }
         },
     });
 
@@ -44,10 +62,10 @@ function Doctors(props) {
     const handleInsert = (values) => {
         let localData = JSON.parse(localStorage.getItem("doctors"));
 
-        let id = Math.floor(Math.random() *10000);
+        let id = Math.floor(Math.random() * 10000);
 
         let data = {
-            id : id,
+            id: id,
             ...values
         }
 
@@ -68,25 +86,80 @@ function Doctors(props) {
         { field: 'email', headerName: 'email', width: 250 },
         { field: 'phone', headerName: 'phone', width: 170 },
         {
-            field : "action" , 
-            headerName : "Action",
+            field: "action",
+            headerName: "Action",
             renderCell: (params) => (
-                console.log(params)
+                <>
+                    <IconButton aria-label="delete" onClick={() => { handleDClickOpen(); setDid(params.id) }}>
+                        <DeleteIcon />
+                    </IconButton>
+                    <IconButton aria-label="delete" onClick={() => handleEdit(params)}>
+                        <EditIcon />
+                    </IconButton>
+                </>
             )
         }
     ];
 
-    const LoadData  = () => {
+    const handleDelete = (params) => {
         let localData = JSON.parse(localStorage.getItem("doctors"));
 
-        if(localData !== null){
+        let fData = localData.filter((d) => d.id !== did);
+
+        localStorage.setItem("doctors", JSON.stringify(fData));
+        console.log(fData);
+        LoadData();
+        handleClose();
+    }
+
+    const handleEdit = (params) => {
+        handleClickOpen();
+
+        setUpdate(true);
+        formik.setValues(params.row)
+    }
+
+    const handleClickUpdate = () => {
+        let localData = JSON.parse(localStorage.getItem("doctors"));
+
+        let uData = localData.map((u) => {
+            if (u.id === values.id) {
+                return values;
+            } else {
+                return u;
+            }
+        })
+        console.log(uData);
+        localStorage.setItem("doctors", JSON.stringify(uData));
+        handleClose();
+        LoadData();
+    }
+
+    const handleSearch = (val) => {
+        let localData = JSON.parse(localStorage.getItem("doctors"));
+
+        let sData = localData.filter((s) => (
+            s.name.toLowerCase().includes(val.toLowerCase()) ||
+            s.email.includes(val.toLowerCase()) ||
+            s.phone.toString().includes(val)
+        ));
+        console.log(sData);
+        setSerch(sData);
+    }
+
+    const finalData = search.length > 0 ? search : data ;
+
+    const LoadData = () => {
+        let localData = JSON.parse(localStorage.getItem("doctors"));
+
+        if (localData !== null) {
             setData(localData);
         }
     }
 
     useEffect(() => {
         LoadData();
-    },[])
+    }, [])
 
 
     return (
@@ -96,9 +169,34 @@ function Doctors(props) {
                 <Button variant="outlined" onClick={handleClickOpen}>
                     Add Doctors List
                 </Button>
+                <TextField
+                    name='search'
+                    id="name"
+                    label="Doctors search"
+                    type="search"
+                    fullWidth
+                    variant="standard"
+                    onChange={(e) => handleSearch(e.target.value)}
+                />
+                <Dialog
+                    open={dopen}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        {"Are You Sure Delete?"}
+                    </DialogTitle>
+                    <DialogActions>
+                        <Button onClick={handleClose}>No</Button>
+                        <Button onClick={handleDelete} autoFocus>
+                            Yes
+                        </Button>
+                    </DialogActions>
+                </Dialog>
                 <div style={{ height: 400, width: '100%' }}>
                     <DataGrid
-                        rows={data}
+                        rows={finalData}
                         columns={columns}
                         pageSize={5}
                         rowsPerPageOptions={[5]}
@@ -111,8 +209,7 @@ function Doctors(props) {
                         <Form onSubmit={handleSubmit}>
                             <DialogContent>
                                 <TextField
-                                    autoFocus
-                                    margin="dense"
+                                    value={values.name}
                                     name='name'
                                     id="name"
                                     label="Doctors Name"
@@ -124,8 +221,7 @@ function Doctors(props) {
                                 />
                                 {errors.name && touched.name ? <p>{errors.name}</p> : ''}
                                 <TextField
-                                    autoFocus
-                                    margin="dense"
+                                    value={values.email}
                                     name='email'
                                     id="name"
                                     label="Email Address"
@@ -137,8 +233,7 @@ function Doctors(props) {
                                 />
                                 {errors.email && touched.email ? <p>{errors.email}</p> : ''}
                                 <TextField
-                                    autoFocus
-                                    margin="dense"
+                                    value={values.phone}
                                     name='phone'
                                     id="name"
                                     label="Doctors Phone"
@@ -151,7 +246,12 @@ function Doctors(props) {
                                 {errors.phone && touched.phone ? <p>{errors.phone}</p> : ''}
                                 <DialogActions>
                                     <Button onClick={handleClose}>Cancel</Button>
-                                    <Button type='submit'>Submit</Button>
+                                    {
+                                        update ?
+                                            <Button type='submit'>update</Button>
+                                            :
+                                            <Button type='submit'>Submit</Button>
+                                    }
                                 </DialogActions>
                             </DialogContent>
                         </Form>
